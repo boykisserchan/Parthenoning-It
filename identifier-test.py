2bc4c32305bec238aba6a21a41a7d95a2cb8892f
astra-the-boop <astra.the.boop@icloud.com>
2025-11-15 17:36:17 -0500

    kills self

--- identifier-test.py
+++ identifier-test.py
@@ -1,3 +1,14 @@
-from getmac import get_mac_address as gma
-mac = gma()
-print(mac)
+# from getmac import get_mac_address as gma
+# mac = gma()
+# print(mac)
+
+
+import dotenv
+import airtable
+import socket
+import time
+
+def findHost(timeout=1):
+    socky = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socky.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socky.bind(("", port))
